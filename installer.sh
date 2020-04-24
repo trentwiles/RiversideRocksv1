89eb0d426dafd9413272f3fb8370ccee23e04922
Glitch (riverside) <none>
2020-04-24 18:39:56 +0000

    Update Installer.sh

--- installer.sh
+++ installer.sh
@@ -1,3 +1,12 @@
-#!/bin/bash
+
+echo Welcome to the Riverside Rocks Website Installer. This installer will
+echo clone the repo (RiversideRocks/RiversideRocks) to your computer and run
+echo server.js. To cancel use CTRL + C
+echo Getting ready...
+sleep 5
 
 git clone https://github.com/RiversideRocks/RiversideRocks.git
+
+sleep 3
+
+node server.js
